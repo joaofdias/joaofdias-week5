38ccbb427d9b2402566970c8791ee1d5de46c173
joaofdias <joaofdias@gmail.com>
2018-10-06 11:17:59 -0300

    Starting project

---
+++ src/main/java/com/example/demo/JoaofdiasWeek5Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaofdiasWeek5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoaofdiasWeek5Application.class, args);
+	}
+}
